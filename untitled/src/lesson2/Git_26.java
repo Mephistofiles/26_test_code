88aeea9d0f076da3b2c3cfdeaae59adf1eed4fe7
Mephistofiles <nikita51379@gmail.com>
2023-05-27 22:19:36 +0300

    test_git

---
+++ untitled/src/lesson2/Git_26.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class Git_26 {
+    public static void main(String[] args) {
+        take();
+    }
+
+    private static void take() {
+        System.out.println("решение");
+    }
+}
